66d3c672eeabcbec792e79689e4246922360bf08
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-02-17 19:29:55 +0100

    feat(Receipt): crear la clase receipt con la implementacion de los metodos de ticket y sus atributos
    falta implementar los metodos relacionados con la clase Extra

---
+++ src/main/java/edu/poniperro/galleygrub/receipt/Receipt.java
@@ -0,0 +1,39 @@
+package edu.poniperro.galleygrub.receipt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.poniperro.galleygrub.order.Comanda;
+import edu.poniperro.galleygrub.order.Order;
+
+public class Receipt implements Ticket {
+    private Double total = 0d;
+    private Comanda order;
+
+    public Receipt(Comanda order) {
+        this.order = order;
+    }
+
+    @Override
+    public Comanda getOrder() {
+        return this.order;
+    }
+
+    @Override
+    public Double total() {
+        return this.total;
+    }
+
+    @Override
+    public void sumExtrasCharge() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void print() {
+        getOrder().display();
+        System.out.println("\tTOTAL--------> " + total() + "$");      
+    }
+    
+}
