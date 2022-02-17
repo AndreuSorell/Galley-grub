c8c5fa77d38b96be15305fdf77d7f45d93c12de8
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-02-17 17:04:43 +0100

    build: Configuracion inicial

---
+++ src/test/java/edu/poniperro/galleygrub/AppTest.java
@@ -0,0 +1,12 @@
+package edu.poniperro.galleygrub;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
