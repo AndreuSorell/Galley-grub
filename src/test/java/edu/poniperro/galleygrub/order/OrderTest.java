65a514f3d5e3ec2f4ba2a84b3e12008563d2246e
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-02-17 19:27:28 +0100

    test(Order): comprobar que el constructor y el metodo updatetotal

--- src/test/java/edu/poniperro/galleygrub/order/OrderTest.java
+++ src/test/java/edu/poniperro/galleygrub/order/OrderTest.java
@@ -1,15 +1,25 @@
 package edu.poniperro.galleygrub.order;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class OrderTest {
+    private Comanda order;
+    @Before
+    public void setupOrder() {
+        order = new Order();
+    }
+    
+    @Test
+    public void constructorTest() {
+        assertNotNull("order creada", order);
+    }
 
     @Test
     public void addItemTest() {
-        Comanda order = new Order();
-
         order.addItem("Krabby Patty", 1.25);
         order.addItem("Coral Bits", 1.00);
         order.addItem("Kelp Rings", 1.50);
@@ -17,5 +27,11 @@ public class OrderTest {
         order.addItem("Seafoam Soda", 1.00);
 
         assertEquals(5, order.size());
-    }    
+    }
+
+    @Test
+    public void updateTotalTest() {
+        order.updateTotal(0.25);
+        assertEquals(0.25, order.getTotal(), 0);
+    }
 }
