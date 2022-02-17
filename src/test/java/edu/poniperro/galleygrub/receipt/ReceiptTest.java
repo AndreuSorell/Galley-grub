0c404520e131f127819b21f674af86837e820edd
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-02-17 21:11:18 +0100

    test(Receipt): eliminar los test que no he utilizado

--- src/test/java/edu/poniperro/galleygrub/receipt/ReceiptTest.java
+++ src/test/java/edu/poniperro/galleygrub/receipt/ReceiptTest.java
@@ -40,52 +40,8 @@ public class ReceiptTest {
     @Test
     public void sum_extra_cheese_test() {
         
-        /* cheese = new CheeseExtra();
-        receipt.setChain(cheese);   
-        assertNotNull(receipt.getChain()); */
-
         Comanda savedOrder = receipt.getOrder();
         assertNotNull(savedOrder);
         assertEquals(0d, savedOrder.getTotal(), 0d);
-
-        /* receipt.sumExtrasCharge();
-        assertEquals(0.25d, receipt.getOrder().getTotal(), 0d); */
     }
-
-    // integracion receipt con tres Extra
-    /* @Test
-    public void chain_test() {
-        
-        cheese = new CheeseExtra();
-        Extra sauce = new SauceExtra();
-        Extra size = new SizeLargeExtra();
-
-        cheese.setNextExtra(sauce);
-        sauce.setNextExtra(size);
-        
-        receipt.setChain(cheese);   
-        assertNotNull(receipt.getChain());
-
-        receipt.sumExtrasCharge();
-        assertEquals(2.25d, receipt.getOrder().getTotal(), 0d);
-    }
-
-    // Total con regular y extras
-    @Test
-    public void total_w_extra_test() {
-
-        cheese = new CheeseExtra();
-        Extra sauce = new SauceExtra();
-        Extra size = new SizeLargeExtra();
-
-        Regular regular = new Regular();
-        regular.setNextExtra(cheese);
-        cheese.setNextExtra(sauce);
-        sauce.setNextExtra(size);
-        
-        receipt.setChain(regular);   
-        assertNotNull(receipt.getChain());
-
-        assertEquals(10.0d , receipt.total(), 0.1);
-    } */
 }
